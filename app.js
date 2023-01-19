48d4db72132282777f82e0c0b972aa8b5d9c4650
alvine2200 <alvinellavu@gmail.com>
2023-01-19 19:10:17 +0300

    finalize

--- app.js
+++ app.js
@@ -5,33 +5,10 @@ const connectDB = require("./databse/connection");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello home");
-});
-
+app.use(express.static("/public"));
 app.use(express.json());
 //get all tasks
 app.use("/api/v1/tasks", tasks);
-
-//create new tasks
-app.post("/api/vi/tasks", (req, res) => {
-  //perform tasks here
-});
-
-//edit tasks
-app.get("api/v1/tasks/:id", (req, res) => {
-  //perform tasks here
-});
-
-//update tasks
-app.put("api/v1/tasks/:id", (req, res) => {
-  //perform tasks here
-});
-
-//delete task
-app.delete("api/v1/tasks/:id", (req, res) => {
-  //perform tasks here
-});
 
 const start = async () => {
   try {
