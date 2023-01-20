6a130b8a3f11d0f63f2bc7af2b1078c947340f65
alvine2200 <alvinellavu@gmail.com>
2023-01-20 17:09:55 +0300

    token found

--- Authentication/controllers/AuthenticationController.js
+++ Authentication/controllers/AuthenticationController.js
@@ -1,15 +1,33 @@
 const User = require("../models/AuthenticationModel");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   res.send("register fields here...");
 };
 
 const login = async (req, res) => {
-  res.send("login fields over here...");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(500)
+      .json({ status: "fail", message: "Please provide email and password" });
+  }
+  const id = new Date().getTime();
+  const token = jwt.sign({ id, email }, process.env.JWT_TOKEN, {
+    expiresIn: "30d",
+  });
+  console.log(token);
+  return res
+    .status(200)
+    .json({ status: "success", message: "user logged in", token: `${token}` });
 };
 
 const dashboard = async (req, res) => {
-  res.send("Welcome to dashboard lad ...");
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json(`Not Auhorized to view this page`);
+  }
+  return res.status(200).json({ status: "success", message: "token found" });
 };
 
 module.exports = { register, login, dashboard };
